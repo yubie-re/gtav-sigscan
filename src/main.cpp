99da3beffec2a259ae5d281126c8ccfe2bb44f0a
yubie <24496817+yubie-re@users.noreply.github.com>
2022-04-06 16:34:38 -0400

    format

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "inc.hpp"
 
-#define XOR_KEY 0xB7AC4B1C
+#define XOR_KEY 0xb7ac4b1c
 
-int rockstar_hash_func(uint8_t* input, size_t size, unsigned int key) // Some form of CRC?
+int rockstar_hash_func(uint8_t *input, size_t size, unsigned int key) // Some form of CRC?
 {
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         key = ((uint32_t)(1025 * (input[i] + key)) >> 6) ^ (1025 * (input[i] + (uint32_t)key));
     return 0x8001 * (((uint32_t)(9 * key) >> 11) ^ (9 * key));
 }
@@ -18,24 +18,23 @@ struct sig
     uint32_t m_protect_flag;
     uint32_t m_size;
     uint32_t m_unk;
-    uint32_t m_report_id;
     uint32_t m_game_version;
+    uint32_t m_unk_two;
 
     sig(std::vector<int32_t> data)
     {
         m_hash = data[3];
         auto xor_const = XOR_KEY ^ m_hash;
-        m_start_byte = (xor_const ^ data[2]) >> 24 & 0xFF;
+        m_start_byte = (xor_const ^ data[2]) >> 24 & 0xff;
         m_start_page = (xor_const ^ data[1]) & 0xffff;
         m_end_page = (xor_const ^ data[1]) >> 16 & 0xffff;
-        m_protect_flag =  (xor_const ^ data[4]) >> 8; // PAGE_READONLY, PAGE_EXECUTE_READWRITE
-        m_size =  (xor_const ^ data[2]) >> 18 & 0x3F;
-        m_unk = (xor_const ^ data[2]) & 0x3FFFF;
-        m_report_id = (xor_const ^ data[0]);
-        m_game_version = (m_hash ^ data[0]) & 0xFFFFFF ^ 0xAC4B1C ^ 0x70000;
+        m_protect_flag = (xor_const ^ data[4]) >> 8; // PAGE_READONLY, PAGE_EXECUTE_READWRITE
+        m_size = (xor_const ^ data[2]) >> 18 & 0x3f;
+        m_unk = (xor_const ^ data[2]) & 0x3ffff;
+        m_game_version = (xor_const ^ data[0]) & 0xffff;
     }
 
-    uintptr_t scan(uint8_t* data, size_t size)
+    uintptr_t scan(uint8_t *data, size_t size)
     {
         for (auto ptr = data; ptr < data + size - m_size; ptr++)
         {
@@ -48,21 +47,20 @@ struct sig
     }
 };
 
-bool is_ascii(const std::string& s)
+bool is_ascii(const std::string &s)
 {
-    return !std::any_of(s.begin(), s.end(), [](char c) { 
-        return static_cast<unsigned char>(c) > 127; 
-    });
+    return !std::any_of(s.begin(), s.end(), [](char c)
+                        { return static_cast<unsigned char>(c) > 127; });
 }
 
 rapidjson::Document download_tunables()
 {
     cpr::Response r = cpr::Get(cpr::Url{"http://prod.cloud.rockstargames.com/titles/gta5/pcros/0x1a098062.json"});
-    uint8_t key[] = { 0xf0, 0x6f, 0x12, 0xf4, 0x9b, 0x84, 0x3d, 0xad, 0xe4, 0xa7, 0xbe, 0x05, 0x35, 0x05, 0xb1, 0x9c, 0x9e, 0x41, 0x5c, 0x95, 0xd9, 0x37, 0x53, 0x45, 0x0a, 0x26, 0x91, 0x44, 0xd5, 0x9a, 0x01, 0x15 };
+    uint8_t key[] = {0xf0, 0x6f, 0x12, 0xf4, 0x9b, 0x84, 0x3d, 0xad, 0xe4, 0xa7, 0xbe, 0x05, 0x35, 0x05, 0xb1, 0x9c, 0x9e, 0x41, 0x5c, 0x95, 0xd9, 0x37, 0x53, 0x45, 0x0a, 0x26, 0x91, 0x44, 0xd5, 0x9a, 0x01, 0x15};
     AES aes(AESKeyLength::AES_256);
     auto crypted_chunk = r.text.size() - (r.text.size() % 16);
-    auto out = aes.DecryptECB((uint8_t*)r.text.data(), (uint32_t)crypted_chunk, key);
-    std::string j((char*)out, crypted_chunk);
+    auto out = aes.DecryptECB((uint8_t *)r.text.data(), (uint32_t)crypted_chunk, key);
+    std::string j((char *)out, crypted_chunk);
     j += std::string(r.text.data() + crypted_chunk, (r.text.size() % 16));
     delete[] out;
     rapidjson::Document d;
@@ -70,24 +68,26 @@ rapidjson::Document download_tunables()
     return d;
 }
 
-int safe_get_int(rapidjson::Value& value)
+int safe_get_int(rapidjson::Value &value)
 {
     return value.IsInt() ? value.GetInt() : value.GetUint();
 }
 
-void loop_bonus(rapidjson::Document& doc, uint8_t* data, size_t size, std::string filename)
+void loop_bonus(rapidjson::Document &doc, uint8_t *data, size_t size, std::string filename)
 {
-    for(auto& bonus : doc["bonus"].GetArray())
+    for (auto &bonus : doc["bonus"].GetArray())
     {
         auto values = bonus.GetArray();
         sig s({safe_get_int(values[0]), safe_get_int(values[1]), safe_get_int(values[2]), safe_get_int(values[3]), safe_get_int(values[4])});
-        //if(s.m_game_version != 2545)
-        //    continue;
-        if(auto location = s.scan(data, size))
+        // if(s.m_game_version != 2545)
+        //     continue;
+        if (auto location = s.scan(data, size))
         {
-            auto str = std::string((char*)location, s.m_size);
-            if(is_ascii(str))
-                printf("(%s) \"%s\" (%u) (v%d)\n", filename.c_str(), str.c_str(), s.m_size, s.m_game_version);
+            auto str = std::string((char *)location, s.m_size);
+            if (is_ascii(str))
+            {
+                printf("(%s) \"%s\" (%u) (v%d) (%s)\n", filename.c_str(), str.c_str(), s.m_size, s.m_game_version, s.m_protect_flag == PAGE_READONLY ? "PAGE_READONLY" : "PAGE_EXECUTE_READWRITE");
+            }
             else
             {
                 printf("(%s) { ", filename.c_str());
@@ -102,7 +102,7 @@ void loop_bonus(rapidjson::Document& doc, uint8_t* data, size_t size, std::strin
 int main()
 {
     auto tunables = download_tunables();
-    for (const auto& entry : std::filesystem::recursive_directory_iterator("./files/"))
+    for (const auto &entry : std::filesystem::recursive_directory_iterator("./files/"))
     {
         std::ifstream i(entry.path(), std::ios::binary);
         std::vector<uint8_t> contents((std::istreambuf_iterator<char>(i)), std::istreambuf_iterator<char>());
