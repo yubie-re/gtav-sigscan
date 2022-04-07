28c481fc96ec601370e4604b9b18f7b65af38bf3
yubie <24496817+yubie-re@users.noreply.github.com>
2022-04-07 16:27:59 -0400

    consistency

--- src/inc.hpp
+++ src/inc.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "AES.h"
-#include "cpr/cpr.h"
+#include <AES.h>
+#include <cpr/cpr.h>
 #include <filesystem>
 #define RAPIDJSON_HAS_STDSTRING 1
 #include <rapidjson/document.h>
