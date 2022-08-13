d4bb35c4dc5d5b51ac7a9dad7718d56f59fc72d4
yubie <24496817+yubie-re@users.noreply.github.com>
2022-08-13 00:48:37 -0400

    sha function + key check + more page types + some formatting

--- src/inc.hpp
+++ src/inc.hpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <filesystem>
+#include <unordered_set>
 #include <AES.h>
 #include <cpr/cpr.h>
 #define RAPIDJSON_HAS_STDSTRING 1
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
+#include <TinySHA1.hpp>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include "inc.hpp"
 
+#define GAME_VERISON 2628
 #define XOR_KEY 0xb7ac4b1c
+
+#define PROTECTION_KV(pr) { pr, #pr }
+
+std::unordered_map<uint32_t, std::string_view> protection_type
+{
+    PROTECTION_KV(PAGE_EXECUTE),
+    PROTECTION_KV(PAGE_EXECUTE_READ),
+    PROTECTION_KV(PAGE_EXECUTE_READWRITE ),
+    PROTECTION_KV(PAGE_EXECUTE_WRITECOPY ),
+    PROTECTION_KV(PAGE_NOACCESS ),
+    PROTECTION_KV(PAGE_READONLY),
+    PROTECTION_KV(PAGE_READWRITE),
+    PROTECTION_KV(PAGE_WRITECOPY),
+    PROTECTION_KV(PAGE_TARGETS_INVALID),
+    PROTECTION_KV(PAGE_TARGETS_NO_UPDATE),
+    PROTECTION_KV(PAGE_GUARD),
+    PROTECTION_KV(PAGE_NOCACHE),
+    PROTECTION_KV(PAGE_WRITECOMBINE)
+};
 
 // "a modified JOAAT that is initialized with the CRC-32 polynomial."  - pelecanidae
 uint32_t sig_joaat(uint8_t *input, uint32_t size)
@@ -28,18 +48,19 @@ struct sig
     uint32_t m_size;
     uint32_t m_region_size_estimate;
     uint32_t m_game_version;
+    uint32_t m_xor_const;
 
     sig(std::vector<uint32_t> data)
     {
-        m_hash = data[3]; // joaat hash of scan
-        auto xor_const = XOR_KEY ^ m_hash; // used for decryption of values
-        m_start_byte = (xor_const ^ data[2]) >> 24 & 0xff; // starting byte of scan
-        m_start_page = (xor_const ^ data[1]) & 0xffff; // start page of scan (base_address + 4096 * a1->start_page)
-        m_end_page = (xor_const ^ data[1]) >> 16 & 0xffff; // end page of scan (base_address + 4096 * a1->start_page)
-        m_protect_flag = (xor_const ^ data[4]) >> 8; // protection flag of scanned region, usually PAGE_READONLY or PAGE_EXECUTE_READWRITE, if different it won't scan
-        m_size = (xor_const ^ data[2]) >> 18 & 0x3f; // length of scanned string or bytes
-        m_region_size_estimate = ((xor_const ^ data[2]) & 0x3ffff) << 10; // region_size > m_region_size_estimate * 0.9 && region_size < m_region_size_estimate * 1.1, otherwise scan doesn't run.
-        m_game_version = (xor_const ^ data[0]) & 0xffff; // game version when scan was added, if game version differs scan will not run
+        m_hash = data[3];                                                   // joaat hash of scan
+        m_xor_const = XOR_KEY ^ m_hash;                                     // used for decryption of values
+        m_start_byte = (m_xor_const ^ data[2]) >> 24 & 0xff;                // starting byte of scan
+        m_start_page = (m_xor_const ^ data[1]) & 0xffff;                    // start page of scan (base_address + 4096 * a1->start_page)
+        m_end_page = (m_xor_const ^ data[1]) >> 16 & 0xffff;                // end page of scan (base_address + 4096 * a1->start_page)
+        m_protect_flag = (m_xor_const ^ data[4]) >> 8;                      // protection flag of scanned region, usually PAGE_READONLY or PAGE_EXECUTE_READWRITE, if different it won't scan
+        m_size = (m_xor_const ^ data[2]) >> 18 & 0x3f;                      // length of scanned string or bytes
+        m_region_size_estimate = ((m_xor_const ^ data[2]) & 0x3ffff) << 10; // region_size > m_region_size_estimate * 0.9 && region_size < m_region_size_estimate * 1.1, otherwise scan doesn't run.
+        m_game_version = (m_xor_const ^ data[0]) & 0xffff;                  // game version when scan was added, if game version differs scan will not run
     }
 
     uint8_t *scan(uint8_t *data, size_t size)
@@ -57,8 +78,7 @@ struct sig
 
 bool is_ascii(uint8_t *start, uint32_t size)
 {
-    return !std::any_of(start, start + size, [](uint8_t c)
-                        { return c > 127; });
+    return !std::any_of(start, start + size, [](uint8_t c) { return c > 127; });
 }
 
 uint32_t safe_get_uint(rapidjson::Value &value)
@@ -83,25 +103,53 @@ rapidjson::Document download_tunables()
 
 void loop_bonus(rapidjson::Document &doc, uint8_t *data, size_t size, std::string filename)
 {
+    if(!doc.HasMember("bonus"))
+        return;
     for (auto &bonus : doc["bonus"].GetArray())
     {
         auto values = bonus.GetArray();
         sig s({safe_get_uint(values[0]), safe_get_uint(values[1]), safe_get_uint(values[2]), safe_get_uint(values[3]), safe_get_uint(values[4])});
-        //if(s.m_game_version != 2545)
+        // if(s.m_game_version != GAME_VERISON)
         //     continue;
         if (auto location = s.scan(data, size))
         {
             if (is_ascii(location, s.m_size))
-                printf("(%s) \"%.*s\" (%u) (v%d) (%s) (~%.3f kb region)\n", filename.c_str(), s.m_size, location, s.m_size, s.m_game_version, s.m_protect_flag == PAGE_READONLY ? "PAGE_READONLY" : "PAGE_EXECUTE_READWRITE", s.m_region_size_estimate / 1000.0);
+                printf("(%s) \"%.*s\" (%u) (v%d) (%s) (~%.3f kb region)\n", filename.c_str(), s.m_size, location, s.m_size, s.m_game_version,  protection_type.contains(s.m_protect_flag) ? protection_type[s.m_protect_flag].data() : ("PAGE_UNK" + std::to_string(s.m_protect_flag)).c_str(), s.m_region_size_estimate / 1000.0);
             else
             {
                 printf("(%s) { ", filename.c_str());
                 for (auto i = 0ull; i < s.m_size; i++)
                     printf("%02hhx ", location[i]);
-                printf("} (%u) (v%d) (%s) (~%.3f kb region)\n", s.m_size, s.m_game_version, s.m_protect_flag == PAGE_READONLY ? "PAGE_READONLY" : "PAGE_EXECUTE_READWRITE", s.m_region_size_estimate / 1000.0);
+                printf("} (%u) (v%d) (%s) (~%.3f kb region)\n", s.m_size, s.m_game_version, protection_type.contains(s.m_protect_flag) ? protection_type[s.m_protect_flag].data() : ("PAGE_UNK" + std::to_string(s.m_protect_flag)).c_str(), s.m_region_size_estimate / 1000.0);
             }
         }
     }
+}
+
+// Used to verify the integrity of the bonus struct once loaded in game.
+std::array<uint8_t, 20> get_bonus_sha(rapidjson::Document &doc)
+{
+    sha1::SHA1 s;
+    uint32_t sig_count = 0;
+    auto sha_update = [&](uint32_t data) { s.processBytes(&data, 4); };
+    sha_update(GAME_VERISON);
+    for (auto &bonus : doc["bonus"].GetArray())
+    {
+        auto values = bonus.GetArray();
+        sig s({safe_get_uint(values[0]), safe_get_uint(values[1]), safe_get_uint(values[2]), safe_get_uint(values[3]), safe_get_uint(values[4])});
+        if (s.m_game_version == GAME_VERISON)
+        {
+            sha_update(safe_get_uint(values[0]));
+            sha_update(safe_get_uint(values[1]));
+            sha_update(safe_get_uint(values[2]));
+            sha_update(safe_get_uint(values[3]));
+            sha_update((safe_get_uint(values[4]) ^ XOR_KEY ^ safe_get_uint(values[3])) & 0xFFFFFF);
+            sig_count++;
+        }
+    }
+    sha_update(sig_count);
+    std::array<uint8_t, 20> hash;
+    s.getDigestBytes(hash.data());
 }
 
 int main()
