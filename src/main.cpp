6edcc173bb0023fdf3389b9f9acc763b28385187
yubie <24496817+yubie-re@users.noreply.github.com>
2024-08-24 20:41:18 -0400

    change multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@ NG::ArxanKey g_decKey            = { 0xb1, 0x51, 0x81, 0x7b, 0xcc, 0xa7, 0xed, 0
 NG::ArxanKey g_integrityCheckKey = { 0xb1, 0x51, 0x81, 0x7b, 0xcc, 0xa7, 0xed, 0xae, 0x23, 0xa8, 0x6d, 0x03, 0x8b, 0x7e, 0x43, 0x2f, 0x9c, 0xa7, 0x06, 0x1a, 0xf8, 0xe9, 0x4e, 0x9c, 0x12, 0xf4, 0x42, 0xc5, 0x73, 0x5e, 0x3e, 0xb6, 0xaa, 0xba, 0xc8, 0xc3, 0xa9, 0x85, 0xe7, 0x78, 0xb8, 0x20, 0x99, 0x16, 0x39, 0xba, 0xfa, 0x6e, 0xd4, 0x5c, 0xd6, 0x8a, 0xf7, 0xef, 0x66, 0x85, 0x23, 0x20, 0xfe, 0x4b, 0x6f, 0x61, 0xe7, 0xb2, 0xbd, 0xed, 0x1d, 0xcf, 0xf4, 0xa1, 0x1b, 0x27, 0x3b, 0x5b, 0x77, 0x77, 0x0c, 0x0a, 0x53, 0x26, 0x15, 0x47, 0x37, 0x35, 0x26, 0xd3, 0x50, 0x23, 0x1e, 0xce, 0xa2, 0x1a, 0xa2, 0x02, 0x69, 0x97, 0xd4, 0x0f, 0x40, 0x81, 0xd5, 0x76, 0x1e, 0x61, 0x8e, 0xcb, 0x23, 0x00, 0x4c, 0x6a, 0xa1, 0x5b, 0x26, 0x63, 0xf0, 0x34, 0x81, 0x88, 0x7d, 0x5a, 0x4f, 0x4b, 0x4f, 0xef, 0x58, 0x9c, 0xdd, 0x1e, 0x88, 0x66, 0x41, 0x8d, 0x52, 0x6f, 0x34, 0x7d, 0xd2, 0xf7, 0xab, 0x62, 0x9a, 0x46, 0xf0, 0x2f, 0x6f, 0x50, 0x42, 0x98, 0x53, 0x8e, 0x23, 0xec, 0x6d, 0xb6, 0x7b, 0x25, 0x2f, 0xb8, 0x71, 0xd2, 0x7e, 0x8c, 0x34, 0x54, 0x97, 0x25, 0x9a, 0x01, 0x1b, 0x5d, 0xd8, 0xac, 0x87, 0xbc, 0xe5, 0xce, 0x5b, 0xc9, 0x62, 0x60, 0xd8, 0x8b, 0xb8, 0x03, 0xcf, 0xcc, 0xb0, 0xbe, 0x40, 0x5d, 0x8a, 0x16, 0x7a, 0xd7, 0x48, 0x50, 0xe8, 0x3c, 0xf1, 0xd6, 0x13, 0xe1, 0x46, 0xa6, 0x94, 0x7d, 0x17, 0x72, 0x09, 0x24, 0xb8, 0x29, 0x8e, 0x94, 0x2f, 0x16, 0x7f, 0xc5, 0xe4, 0xad, 0xbd, 0xc3, 0xf2, 0x6b, 0x8c, 0x84, 0xd9, 0x1c, 0x0e, 0x7a, 0x4f, 0x7c, 0x5f, 0x27, 0x4c, 0x3a, 0x26, 0xe2, 0x68, 0x6e, 0xe0, 0xac, 0x6e, 0x23, 0x8a, 0x2e, 0xe3, 0x19, 0x80, 0x8a, 0xee, 0xbb, 0xc7, 0xeb, 0x2f, 0xf9, 0x6f, 0x51, 0x8c, 0x9e, 0x9f, 0xa6, 0x63, 0x06, 0x00, 0x18, 0xa1, 0x9b, 0xf6, 0xcf, 0x24, 0x3f };
 NG::ArxanKey g_rtmaKey           = { 0xb1, 0x51, 0x81, 0x7b, 0xcc, 0xa7, 0xed, 0xae, 0x23, 0xa8, 0x6d, 0x03, 0x8b, 0x7e, 0x43, 0x2f, 0x1c, 0xd0, 0x36, 0x00, 0xb0, 0x49, 0xfc, 0xd1, 0xa7, 0xc4, 0x01, 0xe0, 0x86, 0x0f, 0x08, 0x64, 0xa2, 0xf1, 0x9b, 0x0f, 0x40, 0xdd, 0xf9, 0xda, 0x27, 0x3d, 0x73, 0x98, 0x7d, 0xdc, 0xc9, 0xa2, 0xb5, 0xe2, 0xec, 0x79, 0x81, 0xec, 0x72, 0x22, 0x7c, 0x10, 0xa6, 0x8c, 0x4e, 0xd3, 0x76, 0x95, 0x40, 0xa9, 0x1f, 0x45, 0x15, 0xb2, 0x56, 0x49, 0x4d, 0x1e, 0x83, 0x5b, 0xd7, 0x71, 0x8a, 0x64, 0x02, 0x61, 0x3d, 0x51, 0x31, 0x6e, 0x7e, 0x77, 0x37, 0xfd, 0xee, 0x7a, 0xdc, 0x80, 0xa0, 0x77, 0xff, 0xb3, 0x74, 0x60, 0xc9, 0x21, 0x51, 0x85, 0x19, 0x9d, 0x9a, 0x42, 0xe1, 0x54, 0x8c, 0x35, 0xd4, 0xa5, 0x2a, 0x68, 0x81, 0x13, 0x59, 0x6a, 0x71, 0xc5, 0x2d, 0xdb, 0x0f, 0x2d, 0x58, 0x9e, 0x27, 0x76, 0x71, 0x28, 0x65, 0x84, 0x4f, 0x78, 0x59, 0xf6, 0x5d, 0xc4, 0xa0, 0x2e, 0x64, 0x03, 0x7d, 0xf5, 0xf4, 0xcc, 0xa1, 0xd3, 0xdd, 0x80, 0x53, 0xa3, 0x3d, 0x21, 0x46, 0x87, 0x96, 0x66, 0xbe, 0x45, 0xd3, 0xb5, 0x0f, 0xde, 0xd1, 0xa1, 0xa7, 0xb7, 0x55, 0x0f, 0x36, 0xd5, 0x87, 0x44, 0xc4, 0x67, 0xaf, 0xa7, 0x38, 0x73, 0xf0, 0x3b, 0x03, 0x84, 0x08, 0xd6, 0x17, 0x77, 0x2b, 0xa6, 0x57, 0x5e, 0x14, 0x55, 0xb0, 0x0e, 0x5d, 0x97, 0x37, 0xf0, 0x80, 0xa5, 0x99, 0xfe, 0xf8, 0x5b, 0xd7, 0x42, 0x88, 0xb0, 0xf1, 0xc2, 0xaa, 0xe9, 0x53, 0x75, 0x14, 0xfd, 0x26, 0xa1, 0xeb, 0xb3, 0xf3, 0x42, 0x0f, 0xe3, 0x7b, 0xc1, 0xbf, 0x38, 0x23, 0x04, 0x26, 0x78, 0x0f, 0x70, 0x41, 0x44, 0xe0, 0xac, 0x6e, 0x23, 0x8a, 0x2e, 0xe3, 0x19, 0x80, 0x8a, 0xee, 0xbb, 0xc7, 0xeb, 0x2f, 0xf9, 0x00, 0x5c, 0xb3, 0x08, 0xdf, 0xd4, 0xdd, 0xfa, 0x34, 0xa6, 0x83, 0xb3, 0x49, 0x9b, 0xd5, 0x6f };
 
+#define THR_COUNT 24
+
 struct ScanJob
 {
     uint8_t m_firstByte;
@@ -69,10 +71,17 @@ static_assert(sizeof(IntegSig) == 0x20);
 static_assert(sizeof(RTMASig) == 0x20);
 
 std::vector<RTMASig> g_rtmaSigs;
+std::array<std::thread, THR_COUNT> g_workers;
 std::vector<IntegSig> g_integrityChecks;
 std::unordered_map<uint32_t, std::string> g_hashMap;
 std::recursive_mutex g_insertionMutex;
+std::recursive_mutex g_jobMutex;
 std::vector<std::thread> g_openThreads;
+std::vector<std::vector<uint8_t>> g_loadedFiles; // contains file contents
+std::vector<std::filesystem::path> g_loadedFilePaths; // contains file contents
+std::queue<std::pair<size_t, RTMASig>> g_rtmaJobs; // index in g_LoadedFiles, sig
+std::queue<std::pair<size_t, IntegSig>> g_integJobs; // index in g_LoadedFiles, sig
+
 
 std::string DownloadTunables()
 {
@@ -125,82 +134,161 @@ uint32_t FNV1a(const uint8_t* input, const uint32_t size)
     return hash;
 }
 
-const uint8_t* ScanBuffer(const uint8_t* data, const size_t size, const ScanJob&& sig)
+size_t ScanBuffer(const std::vector<uint8_t>& data, const ScanJob&& sig)
 {
-    for(const uint8_t* ptr = data; ptr < data + size - sig.m_len; ptr++)
+    for(size_t off = 0; off < data.size() - sig.m_len; off++)
     {
-        if(*ptr != sig.m_firstByte)
+        if(data[off] != sig.m_firstByte)
             continue;
-        if(FNV1a(ptr, sig.m_len) == sig.m_hash)
+        if(FNV1a(data.data() + off, sig.m_len) == sig.m_hash)
         {
-            return ptr;
+            return off;
         }
     }
     return 0;
 }
 
-bool IsAscii(const uint8_t* start, uint32_t size)
+template<typename T, typename T2 = uint8_t>
+bool IsAscii(std::ranges::subrange<T>&& view)
 {
-    return !std::any_of(start, start + size, [](uint8_t c) { return c > 127; });
+    return !std::ranges::any_of(view, [](T2 c) { return c > 127; });
 }
 
-void CheckFile(const uint8_t* data, size_t size, std::filesystem::path filePath)
+void ProcessRTMA(std::vector<uint8_t>& data, std::filesystem::path filePath, RTMASig& signature)
 {
-    for(const RTMASig& signature : g_rtmaSigs)
+    if(data.size() < signature.m_len)
+        return;
+    if(size_t location = ScanBuffer(data, ScanJob({signature.m_firstByte, signature.m_len, signature.m_hash})))
     {
-        g_openThreads.push_back(std::thread([=]{
-            if(const uint8_t* location = ScanBuffer(data, size, ScanJob({signature.m_firstByte, signature.m_len, signature.m_hash})))
+        std::lock_guard<std::recursive_mutex> guard(g_insertionMutex);
+        if(IsAscii(std::ranges::subrange(data.begin() + location, data.begin() + location + signature.m_len)))
+        {
+            g_hashMap[signature.m_hash] = std::string(reinterpret_cast<const char*>(data.data()) + location, signature.m_len);
+            fmt::print("[RTMA] ({}) (~{:.2f}kb) ({:x}-{:x}) \"{}\" ({:d})\n", filePath.filename().string(), (signature.m_moduleSize * 4096) / 1000.f, signature.m_pageLow * 4096, signature.m_pageHigh * 4096, g_hashMap[signature.m_hash], signature.m_len);
+        }
+        else
+        {
+            std::string out = "(Hex) { ";
+            fmt::print("[RTMA] ({}) (~{:.2f}kb) ({:x}-{:x}) ", filePath.filename().string(), (signature.m_moduleSize * 4096) / 1000.f, signature.m_pageLow * 4096, signature.m_pageHigh * 4096);
+            for(size_t i = 0; i < signature.m_len; i++)
             {
-                std::lock_guard<std::recursive_mutex> guard(g_insertionMutex);
-                if(IsAscii(location, signature.m_len))
-                {
-                    g_hashMap[signature.m_hash] = std::string(reinterpret_cast<const char*>(location), signature.m_len);
-                    fmt::print("[RTMA] ({}) (~{:.2f}kb) ({:x}-{:x}) \"{}\" ({:d})\n", filePath.filename().string(), (signature.m_moduleSize * 4096) / 1000.f, signature.m_pageLow * 4096, signature.m_pageHigh * 4096, std::string(reinterpret_cast<const char*>(location), signature.m_len), signature.m_len);
-                }
-                else
-                {
-                    std::string out = "(Hex) { ";
-                    fmt::print("[RTMA] ({}) (~{:.2f}kb) ({:x}-{:x}) ", filePath.filename().string(), (signature.m_moduleSize * 4096) / 1000.f, signature.m_pageLow * 4096, signature.m_pageHigh * 4096);
-                    for(const uint8_t* i = location; i < location + signature.m_len; i++)
-                    {
-                        out += fmt::format("{:02x} ", *i);
-                    }
-                    out += fmt::format("}}", signature.m_len);
-                    g_hashMap[signature.m_hash] = out;
-                    fmt::print("{} ({:d})\n", out, signature.m_len);
-                }
+                out += fmt::format("{:02x} ", data[i]);
             }
-        }));
-        //if(size < signature.m_moduleSize)
-        //    continue;
-        
+            out += fmt::format("}}", signature.m_len);
+            g_hashMap[signature.m_hash] = out;
+            fmt::print("{} ({:d})\n", out, signature.m_len);
+        }
+    }
+}
+
+void ProcessInteg(std::vector<uint8_t>& data, std::filesystem::path filePath, IntegSig& signature)
+{
+    if(data.size() < signature.m_len)
+            return;
+    if(size_t location = ScanBuffer(data, ScanJob({signature.m_firstByte, signature.m_len, signature.m_hash})))
+    {
+        std::lock_guard<std::recursive_mutex> guard(g_insertionMutex);
+        if(IsAscii(std::ranges::subrange(data.begin() + location, data.begin() + location + signature.m_len)))
+        {
+            g_hashMap[signature.m_hash] = std::string(reinterpret_cast<const char*>(data.data()) + location, signature.m_len);
+            fmt::print("[IntegrityCheck] ({}) ({:x}-{:x}) \"{}\" ({:d})\n", filePath.filename().string(), signature.m_pageLow * 4096, signature.m_pageHigh * 4096, g_hashMap[signature.m_hash], signature.m_len);
+        }
+        else
+        {
+            std::string out = "(Hex) { ";
+            fmt::print("[IntegrityCheck] ({}) ({:x}-{:x}) ", filePath.filename().string(), signature.m_pageLow * 4096, signature.m_pageHigh * 4096);
+            for(size_t i = 0; i < signature.m_len; i++)
+            {
+                out += fmt::format("{:02x} ", data[i]);
+            }
+            out += fmt::format("}}", signature.m_len);
+            g_hashMap[signature.m_hash] = out;
+            fmt::print("{} ({:d})\n", out, signature.m_len);
+        }
+    }
+}
+
+
+void WorkerThread()
+{
+    while(true)
+    {
+        g_jobMutex.lock();
+        if(!g_rtmaJobs.empty())
+        {
+            std::pair<size_t, RTMASig> job = g_rtmaJobs.front();
+            g_rtmaJobs.pop();
+            g_jobMutex.unlock();
+            std::vector<uint8_t>& file = g_loadedFiles[job.first];
+            std::filesystem::path path = g_loadedFilePaths[job.first];
+            ProcessRTMA(file, path, job.second);
+        }
+        else
+            g_jobMutex.unlock();
+        g_jobMutex.lock();
+        if(!g_integJobs.empty())
+        {
+            std::pair<size_t, IntegSig> job = g_integJobs.front();
+            g_integJobs.pop();
+            g_jobMutex.unlock();
+            std::vector<uint8_t>& file = g_loadedFiles[job.first];
+            std::filesystem::path path = g_loadedFilePaths[job.first];
+            ProcessInteg(file, path, job.second);
+        }
+        else
+            g_jobMutex.unlock();
+        g_jobMutex.lock();
+        if(g_integJobs.empty() && g_rtmaJobs.empty())
+        {
+            g_jobMutex.unlock();
+            break;
+        }
+        g_jobMutex.unlock();
+        std::this_thread::yield();
+    }
+}
+
+void LoadFile(std::filesystem::path p)
+{
+    std::ifstream i(p, std::ios::binary);
+    i.seekg(0, std::ios::end);
+    std::streamsize size = i.tellg();
+    i.seekg(0, std::ios::beg);
+    if(size <= 10)
+        return;
+    std::vector<uint8_t> contents(size);
+    i.read(reinterpret_cast<char*>(contents.data()), size);
+    g_loadedFiles.push_back(std::move(contents));
+    g_loadedFilePaths.push_back(p);
+    size_t index = g_loadedFiles.size() - 1;
+    for(RTMASig& sig : g_rtmaSigs)
+    {
+        g_rtmaJobs.push(std::make_pair(index, sig));
+    }
+    for(IntegSig& sig : g_integrityChecks)
+    {
+        g_integJobs.push(std::make_pair(index, sig));
+    }
+}
+
+void LoadAllFiles(std::filesystem::path p)
+{
+    for(const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator("./files/"))
+        LoadFile(entry);
+    fmt::print("Loaded files.\n");
+}
+
+void QueueWorkers()
+{
+    for(int i = 0; i < THR_COUNT; i++)
+    {
+        g_workers[i] = std::thread(WorkerThread);
     }
 
-    for(const IntegSig& signature : g_integrityChecks) // These will scan in the GTA Dump.
+    for(auto& thread : g_workers)
     {
-        g_openThreads.push_back(std::thread([=]{
-            if(const uint8_t* location = ScanBuffer(data, size, ScanJob({signature.m_firstByte, signature.m_len, signature.m_hash})))
-            {
-                std::lock_guard<std::recursive_mutex> guard(g_insertionMutex);
-                if(IsAscii(location, signature.m_len))
-                {
-                    g_hashMap[signature.m_hash] = std::string(reinterpret_cast<const char*>(location), signature.m_len);
-                    fmt::print("[IntegrityCheck] ({}) ({:x}-{:x}) \"{}\" ({:d})\n", filePath.filename().string(), signature.m_pageLow * 4096, signature.m_pageHigh * 4096, std::string_view(reinterpret_cast<const char*>(location), signature.m_len), signature.m_len);
-                }
-                else
-                {
-                    std::string out = "(Hex) { ";
-                    fmt::print("[IntegrityCheck] ({}) ({:x}-{:x}) ", filePath.filename().string(), signature.m_pageLow * 4096, signature.m_pageHigh * 4096);
-                    for(const uint8_t* i = location; i < location + signature.m_len; i++)
-                    {
-                        out += fmt::format("{:02x} ", *i);
-                    }
-                    out += fmt::format("}}", signature.m_len);
-                    g_hashMap[signature.m_hash] = out;
-                    fmt::print("{} ({:d})\n", out, signature.m_len);
-                }
-            }
-        }));
+        if(thread.joinable())
+            thread.join();
     }
 }
 
@@ -346,20 +434,6 @@ void DeserializeJSON(const std::string& json)
     }
 }
 
-void LoadFile(std::filesystem::path p)
-{
-    std::ifstream i(p, std::ios::binary);
-    std::vector<uint8_t> contents((std::istreambuf_iterator<char>(i)), std::istreambuf_iterator<char>());
-    CheckFile(contents.data(), contents.size(), p);
-}
-
-void LoadAllFiles(std::filesystem::path p)
-{
-    for(const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator("./files/"))
-    LoadFile(entry);
-        
-}
-
 int main(int argc, const char* args[])
 {
     std::filesystem::create_directories("./files/");
@@ -385,17 +459,14 @@ int main(int argc, const char* args[])
     if(argc == 1)
     {
         LoadAllFiles("./files/");
-        for(auto& thread : g_openThreads)
-                if(thread.joinable())
-                    thread.join();
+        QueueWorkers();
     }
     else if (argc >= 2)
     {
         if(!strcmp(args[1], "-savejson"))
         {
             LoadAllFiles("./files/");
-            for(auto& thread : g_openThreads)
-                thread.join();
+            QueueWorkers();
             std::ofstream f("./signatures.json");
             f << SerializeJSON(*reinterpret_cast<uint32_t*>(data.data())) << std::flush;
             return 0;
@@ -407,16 +478,12 @@ int main(int argc, const char* args[])
            f >> j;
            DeserializeJSON(j);
            LoadAllFiles("./files/");
-           for(auto& thread : g_openThreads)
-                if(thread.joinable())
-                    thread.join();
+           QueueWorkers();
         }
         else
         {
             LoadFile(args[1]);
-            for(auto& thread : g_openThreads)
-                if(thread.joinable())
-                    thread.join();
+            QueueWorkers();
         }
     }
 
